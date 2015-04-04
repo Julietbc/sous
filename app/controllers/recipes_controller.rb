ca6ff814b3ce90fe32682530543bf21a413f6a7d
Juliet Carpenter <julietcarpenter@Juliets-Air.station>
2015-04-04 22:44:22 +1300

    crud halfway for recipe model

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,39 @@
+class RecipesController < ApplicationController
+
+	def index
+		@recipes = Recipe.all
+	end
+
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+
+	def new
+	end
+
+	def create
+		@recipe = Recipe.new(recipe_params)
+
+		 if @recipe.save
+		 	redirect_to @recipe
+		 else
+		 	render 'new'
+		 end
+	end
+
+
+
+
+
+
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:name, :text)
+	end
+end
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403234925) do
+ActiveRecord::Schema.define(version: 20150404070527) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
